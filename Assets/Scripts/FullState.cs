f6042997aeb77c3c8ff9da758c88136da6affd69
yiyuezhuo <yiyuezhuo@gmail.com>
2025-05-27 01:23:24 +0800

    Update XML data and improve tooling

--- Assets/Scripts/FullState.cs
+++ Assets/Scripts/FullState.cs
@@ -11,27 +11,30 @@ public class FullState
     public NavalGameState navalGameState;
     public ViewState viewState;
 
-    static XmlSerializer fullStateSerializer = new XmlSerializer(typeof(FullState));
+    // static XmlSerializer fullStateSerializer = new XmlSerializer(typeof(FullState));
 
     public string ToXML()
     {
-        using (var textWriter = new StringWriter())
-        {
-            using (XmlWriter xmlWriter = XmlWriter.Create(textWriter))
-            {
-                fullStateSerializer.Serialize(xmlWriter, this);
-                string serializedXml = textWriter.ToString();
+        // using (var textWriter = new StringWriter())
+        // {
+        //     using (XmlWriter xmlWriter = XmlWriter.Create(textWriter))
+        //     {
+        //         fullStateSerializer.Serialize(xmlWriter, this);
+        //         string serializedXml = textWriter.ToString();
 
-                return serializedXml;
-            }
-        }
+        //         return serializedXml;
+        //     }
+        // }
+
+        return XmlUtils.ToXML(this);
     }
 
     public static FullState FromXML(string xml)
     {
-        using (var reader = new StringReader(xml))
-        {
-            return (FullState)fullStateSerializer.Deserialize(reader);
-        }
+        // using (var reader = new StringReader(xml))
+        // {
+        //     return (FullState)fullStateSerializer.Deserialize(reader);
+        // }
+        return XmlUtils.FromXML<FullState>(xml);
     }
 }

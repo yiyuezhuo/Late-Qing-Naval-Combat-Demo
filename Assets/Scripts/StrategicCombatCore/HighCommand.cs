61971bae1b1d5905858d0643634a70d923b3957a
yiyuezhuo <yiyuezhuo@gmail.com>
2025-07-21 01:05:16 +0800

    Add more high command leaders

--- Assets/Scripts/StrategicCombatCore/HighCommand.cs
+++ Assets/Scripts/StrategicCombatCore/HighCommand.cs
@@ -28,7 +28,12 @@ namespace StrategicCombatCore
 
     public class ImperialGeneralHeadquarters // Imperial General Headquarters / 大本営
     {
+        // https://shirakaba.link/betula/%E5%A4%A7%E6%9C%AC%E5%96%B6_(%E6%97%A5%E6%B8%85%E6%88%A6%E4%BA%89)
+        public DepartmentPosition aideDeCampToTheEmperor = new(); // Aide-De-Camp to the Emperor / 侍従武官長
 
+        // The above people (Chief of Staff, Deputy chief, ...) are included in the Imperial General Headquarters as well but it's not included them again. 
+
+        public DepartmentPosition directorGeneralOfLogistics = new(); // Director-General of Logistics / 兵站総監
     }
 
 
@@ -41,9 +46,33 @@ namespace StrategicCombatCore
         public ImperialGeneralHeadquarters imperialGeneralHeadquarters = new();
     }
 
+    public class BeiyangLocal // Beiyang / 北洋
+    {
+        public DepartmentPosition beiyangMinister = new(); // Superintendent of Trade for the Northern Ports  / 北洋通商大臣
+        public DepartmentPosition viceroyOfZhili = new(); // Viceroy of Zhili / 直隶总督 
+        public DepartmentPosition generalOfFengTian = new(); // General of Fengtian / 奉天将军
+        public DepartmentPosition governorOfShandong = new(); // Governor of Shandong / 山东巡抚
+        public DepartmentPosition admiralOfTheBeiyangFleet = new(); // Admiral of the Beiyang Fleet / 北洋水师提督
+    }
+
+    public class MinistryOfWarChinese // Ministry of War / 兵部
+    {
+        public DepartmentPosition ministerOfWarManchu = new(); // Minister of War (Manchu) / 兵部尚书 (满缺)
+        public DepartmentPosition ministerOfWarHan = new(); // Minister of War (Han) / 兵部尚书 (汉缺)
+    }
+
+    public class NavalMinistryChinese // Naval Ministry / 总理海军事务衙门
+    {
+        public DepartmentPosition grandMinisterOfTheNavy = new(); // Grand Minister of the Navy / 总理大臣
+        public DepartmentPosition associateMinisterOfTheNavy = new(); // Associate Minister of the Navy / 会办大臣
+        public DepartmentPosition assistantMinisterOfTheNavy = new(); // Assistant Minister of the Navy / 帮办大臣
+    }
+
     public class ChineseHighCommand
     {
-
+        public BeiyangLocal beiyangLocal = new();
+        public MinistryOfWarChinese ministryOfWar = new();
+        public NavalMinistryChinese navalMinistry = new();
     }
 
     public class HighCommand
